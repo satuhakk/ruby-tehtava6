eb5c0d5cfe1f497998c40c4ce531ae854ab069e1
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 19:58:53 +0200

    Lukujen monistus ok

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,13 @@
-# kirjoita koodi tänne
+def muuntohomma(luku)
+  rimpsu = ""
+  1.upto(luku) do |i|
+      rimpsu = rimpsu + luku.to_s
+  end
+  return rimpsu
+end
+
+def monista(taulukko)
+  taulukko.map { |alkio| muuntohomma(alkio)}
+end
+
+monista([1, 5, 3])
